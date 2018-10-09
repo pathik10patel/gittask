cec1a1b19f1dd70f1a638260a9468db39bcf46d2
pathik10patel <pathik10patel@gmail.com>
2018-10-09 10:53:23 +0530

    first commit

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        public static int sum()
+        {
+            int i, sum = 0;
+            for (i = 0; i <= 10; i++)
+            {
+                sum += i;
+            }
+            //Console.Write("The sum of 10 no is : {0}", sum);
+            return sum;
+        }
+
+        public static void Main(string[] args)
+        {
+            int x = sum();
+            Console.WriteLine(x);
+        }
+    }
+}
